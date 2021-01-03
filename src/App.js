5df9ad5c1723f7575ed69db82b22e6b40331b3e8
Darrick-Oliver <51181851+Darrick-Oliver@users.noreply.github.com>
2021-01-03 14:07:32 -0800

    Added scores of games and box score retrieval

--- src/App.js
+++ src/App.js
@@ -1,42 +1,118 @@
-import $ from "jquery";
+import $ from 'jquery';
 import './App.css';
 
 // Currently using nba's website as proxy to get past CORS
 // Find better way to fix this error
 
-function App() {
-  const url='/static/json/liveData/scoreboard/todaysScoreboard_00.json'
+function getGameTime(game) {
+  var timeUTC = game.gameTimeUTC.match(/\d\d:\d\d:\d\d/)[0];
+  var date = new Date();
+  var offset = date.getTimezoneOffset();
+
+  var hours = parseInt(timeUTC.match(/\b\d\d:/)[0].match(/\d\d/)[0]);
+  var minutes = parseInt(timeUTC.match(/:\d\d:/)[0].match(/\d\d/)[0]);
+
+  // Weird but works
+  var totalMins = hours * 60 + minutes - offset;
+  var localHour = Math.floor(totalMins/60);
+  var localMins = totalMins % 60;
+  var period;
+  if (localHour > 12) {
+    localHour = localHour - 12;
+    period = 'pm';
+  }
+  else if (localHour == 12) {
+    period = 'pm';
+  }
+  else if (localHour < 0) {
+    localHour = localHour + 12;
+    period = 'pm';
+  }
+  else {
+    period = 'am';
+  }
+  if (localMins < 0) {
+    localMins = localMins + 60;
+  }
+
+  var localTime;
+  if (localMins < 10) {
+    localTime = localHour + ':0' + localMins + ' ' + period;
+  }
+  else {
+    localTime = localHour + ':' + localMins + ' ' + period;
+  }
+
+  return localTime;
+}
+
+function getStatus(game) {
+  var status = game.gameStatusText;
+  if (status.includes('ET')) {
+    status = getGameTime(game);
+  }
+  return status;
+}
+
+function getBoxScore(game) {
+  var data;
+  console.log(game.gameId);
+  const url='/static/json/liveData/boxscore/boxscore_' + game.gameId + '.json';
+  try {
+    data = requestData(url, false);
+  }
+  catch(err) {
+    console.log(err.name);
+  }
+  console.log(data);
+}
+
+function requestData(url, parse = true) {
   var data;
   $.ajax({
     url: url,
     type: "GET",
     async:false,
     success: function(result) {
-      data = JSON.parse(result).scoreboard;
+      if (parse) {
+        data = JSON.parse(result);
+      }
+      else {
+        data = result;
+      }
     },
     error: function(error) {
-      console.log('Error ' + error)
+      console.log('Error ' + error);
     }
   })
-  console.log(data)
+  return data;
+}
 
-  function getDate() {
-    return data.gameDate;
-  }
 
-  function getGames() {
-    var games = data.games
-  }
+function App() {
+  const url='/static/json/liveData/scoreboard/todaysScoreboard_00.json'
+  var data = requestData(url).scoreboard;
 
   return (
-    <div>
+    <div className="App">
       <div className="top">
-        <h1>Basketball Scores</h1>
-        <p>{getDate()}</p>
+        <h1>NBA Scores</h1>
+        <p>{data.gameDate}</p>
       </div>
       
       <div className="games">
-        <h2></h2>
+        {
+          data.games.map(function(game) {
+            return (
+              <div key={game.gameId}>
+                <a href="#">{game.homeTeam.teamName} vs {game.awayTeam.teamName}</a>
+                <p>{game.homeTeam.score} : {game.awayTeam.score}</p>
+                <h3>{getStatus(game)}</h3>
+                {getBoxScore(game)}
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
