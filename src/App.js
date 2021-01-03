0a81ba96783e33e3beef1b6442291212882037bf
Darrick-Oliver <51181851+Darrick-Oliver@users.noreply.github.com>
2021-01-02 17:36:53 -0800

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import $ from "jquery";
 import './App.css';
 
+// Currently using nba's website as proxy to get past CORS
+// Find better way to fix this error
+
 function App() {
+  const url='/static/json/liveData/scoreboard/todaysScoreboard_00.json'
+  var data;
+  $.ajax({
+    url: url,
+    type: "GET",
+    async:false,
+    success: function(result) {
+      data = JSON.parse(result).scoreboard;
+    },
+    error: function(error) {
+      console.log('Error ' + error)
+    }
+  })
+  console.log(data)
+
+  function getDate() {
+    return data.gameDate;
+  }
+
+  function getGames() {
+    var games = data.games
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="top">
+        <h1>Basketball Scores</h1>
+        <p>{getDate()}</p>
+      </div>
+      
+      <div className="games">
+        <h2></h2>
+      </div>
     </div>
   );
 }
